5426e3981ea18aed6c52195b2ff75ccf09817459
masaki-dot <masaki4196@g.kochinet.ed.jp>
2025-06-29 00:34:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -210,7 +210,7 @@ function shuffle(arr) {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
 
 });
