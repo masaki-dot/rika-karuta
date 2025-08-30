222cd3d0c3754243e32da6a4cd655a1aa16048e3
masaki-dot <masaki4196@g.kochinet.ed.jp>
2025-08-30 18:23:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);const express = require("express");
+const io = new Server(server);
+const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
